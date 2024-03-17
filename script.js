25119541d4c16f72239785ce91e8246e453b49a8
Andrew Bollard <bollard92@gmail.com>
2024-03-17 08:53:03 -0500

    Updated project structure and added script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 for (let x = 0; x < topics.length; x++) {
  console.log(topics[x]);
 }
-/*
+
 if (topic === 'HTML') {
  console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -15,4 +15,3 @@ if (topic === 'HTML') {
 } else {
  console.log('Please try again!');
 }
-*/
